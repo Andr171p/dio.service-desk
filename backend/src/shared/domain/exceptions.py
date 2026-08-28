4ed4768f76ede3d5e49dcbe0c4fb33069693c6fe
AndreyKosov <andrey.kosov.05@inbox.ru>
2026-08-29 00:31:23 +0500

    29.08.2026 add base http service client

--- backend/src/shared/domain/exceptions.py
+++ backend/src/shared/domain/exceptions.py
@@ -6,7 +6,6 @@ class AppError(Exception):
 
     status_code: int = status.HTTP_500_INTERNAL_SERVER_ERROR
     error_code: str = "INTERNAL_SERVER_ERROR"
-    public_message: str = "Внутренняя ошибка сервера"
 
     def __init__(
             self,
@@ -15,7 +14,7 @@ class AppError(Exception):
             error_code: str | None = None,
             details: dict | list | None = None,
     ):
-        self.message = message or self.public_message
+        self.message = message
         self.status_code = status_code or self.status_code
         self.error_code = error_code or self.error_code
         self.details = details or {}
@@ -25,46 +24,38 @@ class AppError(Exception):
 class DatabaseError(AppError):
     status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
     error_code = "DATABASE_ERROR"
-    public_message = "Ошибка сервера базы данных"
 
 
 class NotFoundError(AppError):
     status_code = status.HTTP_404_NOT_FOUND
     error_code = "RESOURCE_NOT_FOUND"
-    public_message = "Ресурс не найден"
 
 
 class InvariantViolationError(AppError):
     status_code = status.HTTP_409_CONFLICT
     error_code = "INVARIANT_VIOLATION"
-    public_message = "Нарушены условия существования объекта"
 
 
 class InvalidStateError(AppError):
     status_code = status.HTTP_409_CONFLICT
     error_code = "INVALID_STATE"
-    public_message = "Невалидное состояние объекта"
 
 
 class AlreadyExistsError(AppError):
     status_code = status.HTTP_409_CONFLICT
     error_code = "ALREADY_EXISTS"
-    public_message = "Ресурс уже существует"
 
 
 class EmailSendingFailedError(AppError):
     status_code = status.HTTP_502_BAD_GATEWAY
     error_code = "EMAIL_SENDING_FAILED"
-    public_message = "Не удалось отправить письмо. Попробуйте позже."
 
 
 class RateLimitExceededError(AppError):
     status_code = status.HTTP_429_TOO_MANY_REQUESTS
     error_code = "RATE_LIMIT_EXCEEDED"
-    public_message = "Превышен лимит запросов"
 
 
 class UnsupportedOperationError(AppError):
     status_code = status.HTTP_405_METHOD_NOT_ALLOWED
     error_code = "UNSUPPORTED_OPERATION"
-    public_message = "Операция удалена или не поддерживается."
